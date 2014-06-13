2a16ef4d2a2b1c40fa2f37541b1f1a3a08f8d206
Olivier Spinelli <olivier.spinelli@invenietis.com>
2014-06-13 17:43:31 +0200

    Updraded LibGit2Sharp.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle( "CK.Stamp" )]
-[assembly: AssemblyProduct( "CK.Stamp" )]
-[assembly: AssemblyVersion( "0.8.1" )]
-[assembly: AssemblyFileVersion( "0.8.1" )]
+[assembly: AssemblyTitle( "CK.Stamp.Fody" )]
+[assembly: AssemblyProduct( "CK.Stamp.Fody" )]
+[assembly: AssemblyVersion( "0.8.2" )]
+[assembly: AssemblyFileVersion( "0.8.2" )]

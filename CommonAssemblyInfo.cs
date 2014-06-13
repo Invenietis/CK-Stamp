74a6e6a2a7a388c86b7abe2fd3a24bcb5c8250bd
Olivier Spinelli <olivier.spinelli@invenietis.com>
2014-06-13 19:01:29 +0200

    0.8.3: nuspec now specifies <developmentDependency>true</developmentDependency>

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle( "CK.Stamp.Fody" )]
 [assembly: AssemblyProduct( "CK.Stamp.Fody" )]
-[assembly: AssemblyVersion( "0.8.2" )]
-[assembly: AssemblyFileVersion( "0.8.2" )]
+[assembly: AssemblyVersion( "0.8.3" )]
+[assembly: AssemblyFileVersion( "0.8.3" )]

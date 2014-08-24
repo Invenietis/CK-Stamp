3ab5a9a38fa9e0e98ebbb78bca74e91507d2783b
Olivier Spinelli <olivier.spinelli@invenietis.com>
2014-08-24 11:12:18 +0200

    Fixed bad merge.
    Removed SharedKey. Version 1.0.1.
    Rem  copying to CK-Releaser 0.8.3

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,7 +1,7 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyProduct( "CK-Releaser" )]
 [assembly: AssemblyCopyright( "Copyright � Invenietis 2014" )]
-[assembly: AssemblyVersion( "1.0.0" )]
+[assembly: AssemblyVersion( "1.0.1" )]
 [assembly: AssemblyInformationalVersion( "1.0.0-master (no auto-stamp)." )]
 [assembly: AssemblyFileVersion( "1.0.1" )]

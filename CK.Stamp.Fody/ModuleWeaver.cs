36cff2fbd3321ed1a03848654e7a372896d56116
Olivier Spinelli <olivier.spinelli@invenietis.com>
2014-08-24 10:00:01 +0200

    Up to here , it seems to work.

---
+++ CK.Stamp.Fody/ModuleWeaver.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using CK.Releaser;
+using LibGit2Sharp;
+using Mono.Cecil;
+using Mono.Collections.Generic;
+
+public class ModuleWeaver
+{
+    public Action<string> LogInfo { get; set; }
+    public Action<string> LogWarning { get; set; }
+    public ModuleDefinition ModuleDefinition { get; set; }
+    public string SolutionDirectoryPath { get; set; }
+    public string AddinDirectoryPath { get; set; }
+    public string AssemblyFilePath { get; set; }
+    static bool isPathSet;
+
+    StandardInfo _info;
+    string _informationalVersion;
+
+    public ModuleWeaver()
+    {
+        LogInfo = s => { };
+        LogWarning = s => { };
+    }
+
+    public void Execute()
+    {
+        SetSearchPath();
+
+        var gitDir = GitDirFinder.TreeWalkForGitDir( SolutionDirectoryPath );
+        using( var repo = gitDir == null ? null : GetRepo( gitDir ) )
+        {
+            _info = new StandardInfo( new PersistentInfo( repo ), ModuleDefinition );
+
+            var customAttributes = ModuleDefinition.Assembly.CustomAttributes;
+            var attr = customAttributes.FirstOrDefault(x => x.AttributeType.Name == "AssemblyInformationalVersionAttribute");
+            if( attr != null )
+            {
+                _informationalVersion = (string)attr.ConstructorArguments[0].Value;
+                _informationalVersion = _info.ReplaceTokens( _informationalVersion );
+                attr.ConstructorArguments[0] = new CustomAttributeArgument( ModuleDefinition.TypeSystem.String, _informationalVersion );
+            }
+            else
+            {
+                var versionAttribute = GetVersionAttribute();
+                var constructor = ModuleDefinition.Import( versionAttribute.Methods.First( x => x.IsConstructor ) );
+                attr = new CustomAttribute( constructor );
+                _informationalVersion = _info.ToString();
+                attr.ConstructorArguments.Add( new CustomAttributeArgument( ModuleDefinition.TypeSystem.String, _informationalVersion ) );
+                customAttributes.Add( attr );
+            }
+        }
+    }
+
+    static Repository GetRepo( string gitDir )
+    {
+        try
+        {
+            return new Repository( gitDir );
+        }
+        catch( Exception exception )
+        {
+            if( exception.Message.Contains( "LibGit2Sharp.Core.NativeMethods" ) || exception.Message.Contains( "FilePathMarshaler" ) )
+            {
+                throw new WeavingException( "Restart of Visual Studio required due to update of 'Stamp.Fody': " + exception.Message );
+            }
+            throw;
+        }
+    }
+
+    void SetSearchPath()
+    {
+        if( isPathSet ) return;
+        isPathSet = true;
+        var nativeBinaries = Path.Combine( AddinDirectoryPath, "NativeBinaries", GetProcessorArchitecture() );
+        var existingPath = Environment.GetEnvironmentVariable( "PATH" );
+        var newPath = string.Concat( nativeBinaries, Path.PathSeparator, existingPath );
+        Environment.SetEnvironmentVariable( "PATH", newPath );
+    }
+
+    static string GetProcessorArchitecture()
+    {
+        if (Environment.Is64BitProcess)
+        {
+            return "amd64";
+        }
+        return "x86";
+    }
+
+    TypeDefinition GetVersionAttribute()
+    {
+        var msCoreLib = ModuleDefinition.AssemblyResolver.Resolve("mscorlib");
+        var msCoreAttribute = msCoreLib.MainModule.Types.FirstOrDefault(x => x.Name == "AssemblyInformationalVersionAttribute");
+        if (msCoreAttribute != null)
+        {
+            return msCoreAttribute;
+        }
+        var systemRuntime = ModuleDefinition.AssemblyResolver.Resolve("System.Runtime");
+        return systemRuntime.MainModule.Types.First(x => x.Name == "AssemblyInformationalVersionAttribute");
+    }
+
+    public void AfterWeaving()
+    {
+        var verPatchPath = Path.Combine( AddinDirectoryPath, "verpatch.exe" );
+        // Product version MUST start with the binary version number.
+        var productVersion = _info.AssemblyVersion.ToString() + " - " + _informationalVersion;
+        var arguments = string.Format( "\"{0}\" /pv \"{1}\" /high /va {2}", AssemblyFilePath, productVersion, _info.AssemblyVersion.ToString() );
+        LogInfo( string.Format( "Patching version using: {0} {1}", verPatchPath, arguments ) );
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = verPatchPath,
+            Arguments = arguments,
+            CreateNoWindow = true,
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            WorkingDirectory = Path.GetTempPath()
+        };
+        using( var process = Process.Start( startInfo ) )
+        {
+            if( !process.WaitForExit( 1000 ) )
+            {
+                var timeoutMessage = string.Format( "Failed to apply product version to Win32 resources in 1 second.\r\nFailed command: {0} {1}", verPatchPath, arguments );
+                throw new WeavingException( timeoutMessage );
+            }
+
+            if( process.ExitCode == 0 )
+            {
+                return;
+            }
+            var output = process.StandardOutput.ReadToEnd();
+            var error = process.StandardError.ReadToEnd();
+            var message = string.Format( "Failed to apply product version to Win32 resources.\r\nOutput: {0}\r\nError: {1}", output, error );
+            throw new WeavingException( message );
+        }
+    }
+}
---
+++ CK.Stamp.Fody/PersistentInfo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using LibGit2Sharp;
+using Mono.Cecil;
+
+namespace CK.Releaser
+{
+    public class PersistentInfo
+    {
+        public readonly string RepositoryError;
+        public readonly bool IsDirty;
+        public readonly BasicVersionOnBranch ReleasedTag;
+        public readonly string BranchName;
+        public readonly string CommitSha;
+        public readonly string UserName;
+
+        public PersistentInfo( Repository r )
+        {
+            UserName = String.IsNullOrWhiteSpace( Environment.UserDomainName )
+                           ? Environment.UserName
+                           : string.Format( @"{0}\{1}", Environment.UserDomainName, Environment.UserName );
+            if( r == null ) RepositoryError = "No repository.";
+            else
+            {
+                var branch = r.Head;
+                if( branch.Tip == null ) RepositoryError = "Unitialized repository.";
+                else
+                {
+                    CommitSha = branch.Tip.Sha;
+                    var repositoryStatus = r.Index.RetrieveStatus();
+                    IsDirty =  repositoryStatus.Added.Any()
+                                || repositoryStatus.Missing.Any()
+                                || repositoryStatus.Modified.Any()
+                                || repositoryStatus.Removed.Any()
+                                || repositoryStatus.Staged.Any();
+                    ReleasedTag = FindReleasedVersion( r, branch.Tip );
+                    if( ReleasedTag.IsValid )
+                    {
+                        BranchName = ReleasedTag.BranchName;
+                    }
+                    else
+                    {
+                        BranchName = branch.Name;
+                    }
+                }
+            }
+        }
+
+        static BasicVersionOnBranch FindReleasedVersion( Repository r, Commit commit )
+        {
+            foreach( var tag in r.Tags )
+            {
+                if( tag.Target.Sha == commit.Sha )
+                {
+                    BasicVersionOnBranch v = BasicVersionOnBranch.TryParse( tag.Name );
+                    if( v.IsValid ) return v;
+                }
+            }
+            return new BasicVersionOnBranch();
+        }
+
+
+    }
+}
